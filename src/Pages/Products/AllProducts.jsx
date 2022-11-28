0ce1daa9c493ead203e82d9aee548bdeace3b298
Oliul-1 <olioliulislam832@gmail.com>
2022-11-28 17:09:27 +0600

    connection chick

--- src/Pages/Products/AllProducts.jsx
+++ src/Pages/Products/AllProducts.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ProductCarts from './ProductCarts';
 
 const AllProducts = () => {
     const data = useLoaderData();
     console.log(data);
+    const [products, setProducts] = useState()
+
+    useEffect(() => {
+        fetch("http://localhost:5000/products")
+            .then(res => setProducts(res))
+            .then(err => (console.log(err)))
+    }, [])
+
     return (
         <div>
             <p>{data.length}</p>
@@ -11,18 +20,19 @@ const AllProducts = () => {
             <div className="drawer drawer-mobile">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-center justify-center">
-                    
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
+                    {
+                        products.map(product => <ProductCarts
+                            key={product._id}
+                            product={product}
+                        ></ProductCarts>)
+                    }
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 bg-slate-200 text-base-content">
 
-                    {data.map(da =><li><button>{da.name}</button></li> )}    
-                    
-                        {/* <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li> */}
+                        {data.map(da => <li><button>{da.name}</button></li>)}
+
                     </ul>
 
                 </div>
---
+++ src/Pages/Products/ProductCarts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProductCarts = ({ product }) => {
+    console.log(product);
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ProductCarts;
